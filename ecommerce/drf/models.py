41b2a57ffc0993e4fd166ffcbb9ae6046f7684af
leoseis <eboscrib2@yahoo.com>
2026-02-26 17:57:34 +0100

    Improve Payment Model

--- ecommerce/drf/models.py
+++ ecommerce/drf/models.py
@@ -168,29 +168,28 @@ class Order(models.Model):
         return f"Order {self.id} - {self.user.username}"
 
 class Payment(models.Model):
-    STATUS_CHOICES = [
-        ("pending", "Pending"),
-        ("success", "Success"),
-        ("failed", "Failed"),
-    ]
-
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    order = models.ForeignKey(
+        "Order",
         on_delete=models.CASCADE,
         related_name="payments"
     )
-    order = models.OneToOneField(
-        Order,
-        on_delete=models.CASCADE,
-        related_name="payment"
-    )
-    reference = models.CharField(max_length=255, unique=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    reference = models.CharField(max_length=255, unique=True)
+
     status = models.CharField(
         max_length=20,
-        choices=STATUS_CHOICES,
+        choices=[
+            ("pending", "Pending"),
+            ("success", "Success"),
+            ("failed", "Failed"),
+        ],
         default="pending"
     )
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
